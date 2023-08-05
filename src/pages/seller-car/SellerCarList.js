188de4ffa6afc1470d46ed6a5c0d5677e97b9ddb
indexofmayank <mayankdevtiwari@gmail.com>
2023-08-05 18:49:06 +0530

    'sellerCarListLittleDone'

--- src/pages/seller-car/SellerCarList.js
+++ src/pages/seller-car/SellerCarList.js
@@ -1,12 +1,109 @@
+import * as React from 'react';
+import { Box, TableSortLabel } from '../../../node_modules/@mui/material/index';
+import { Table } from '../../../node_modules/@mui/material/index';
+import { TableBody } from '../../../node_modules/@mui/material/index';  
+import TableCell from '@mui/material/TableCell';
+import { TableContainer } from '../../../node_modules/@mui/material/index';
+import { TableHead } from '../../../node_modules/@mui/material/index';
+import { TableRow } from '../../../node_modules/@mui/material/index';
+import { orderBy } from 'lodash';
+import { visuallyHidden } from '@mui/utils';
+import { string } from '../../../node_modules/yup/lib/index';
 
-import ListCard from 'components/ListCard';
-import { Button } from '@mui/material';
+
+function createData(
+    name,
+    calories,
+    fat,
+    carbs,
+    protein
+) {
+    return {
+        name,
+        calories,
+        fat,
+        carbs,
+        protein
+    }
+}
+
+
+const rows = [
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Donut', 452, 25.0, 51, 4.9),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Honeycomb', 408, 3.2, 87, 6.5),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Jelly Bean', 375, 0.0, 94, 0.0),
+    createData('KitKat', 518, 26.0, 65, 7.0),
+    createData('Lollipop', 392, 0.2, 98, 0.0),
+    createData('Marshmallow', 318, 0, 81, 2.0),
+    createData('Nougat', 360, 19.0, 9, 37.0),
+    createData('Oreo', 437, 18.0, 63, 4.0),
+];
+
+const headCells = [
+    {
+        id: 'name',
+        numeric: false,
+        disablePadding: true,
+        label: 'Dessert (100g serving)',
+      },
+      {
+        id: 'calories',
+        numeric: true,
+        disablePadding: false,
+        label: 'Calories',
+      },
+      {
+        id: 'fat',
+        numeric: true,
+        disablePadding: false,
+        label: 'Fat (g)',
+      },
+      {
+        id: 'carbs',
+        numeric: true,
+        disablePadding: false,
+        label: 'Carbs (g)',
+      },
+      {
+        id: 'protein',
+        numeric: true,
+        disablePadding: false,
+        label: 'Protein (g)',
+      },
+]
 
 const SellerCarList = () => {
     return (
-        <ListCard title="List" codeHighlight contentSX={{ pl: 12.25, pr: 12.25, minHeight: 'calc(100vh - 320px)' }}>
-            <Button></Button>
-        </ListCard>
+        <TableHead>
+            <TableRow>
+               {headCells.map((headCell) => (
+                <TableCell
+                    key={headCell.id}
+                    align={headCell.numeric ? 'right' : 'left'}
+                    padding={headCell.disablePadding ? 'none': 'normal'}
+                    sortDirection={orderBy === headCell.id ? orderBy : false}
+                >
+                    <TableSortLabel 
+                        active={orderBy === headCell.id}
+                        direction={orderBy === headCell.id ? orderBy : 'asc'}
+                        //onClick is left
+                    >
+                        {headCell.label}
+                        {orderBy === headCell.id ? (
+                            <Box component="span"  sx={visuallyHidden}>
+                                {orderBy === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                            </Box>
+                        ) : null}
+                    </TableSortLabel>
+                </TableCell>
+               ))}
+            </TableRow>
+        </TableHead>
     );
 };
 
